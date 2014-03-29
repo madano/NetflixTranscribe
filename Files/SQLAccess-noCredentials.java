73055168347500053ef204e86d58be1ea8450efb
John(Rob) Mcgarel <r.mcgarel@gmail.com>
2014-03-29 17:29:04 -0500

    Updated sqlaccess
    Code that works with cs department database (old code wasn't tested).
    Only insertions, doesn't include queries. Additional examples /
    tutorial: http://www.vogella.com/tutorials/MySQLJava/article.html

--- Files/SQLAccess-noCredentials.java
+++ Files/SQLAccess-noCredentials.java
@@ -19,26 +19,104 @@ import java.util.Date;
 	      // this will load the MySQL driver, each DB has its own driver
 	      Class.forName("com.mysql.jdbc.Driver");
 	      // setup the connection with the DB.
-	      connect = DriverManager.getConnection("jdbc:mysql://user/databasename?"
-	              + "user=databaseuser&password=databasepwd"); //change credentials to match
+	      connect = DriverManager.getConnection("jdbc:mysql://database2.cs.tamu.edu/databasename?" + "user=mysqluser&password=mysqlpw");
 
 	      // statements allow to issue SQL queries to the database
 	      statement = connect.createStatement();
 	      // resultSet gets the result of the SQL query
-	      resultSet = statement.executeQuery("select * from rmcgarel.Customer");
-	      writeResultSet(resultSet);
-	      for (int i = 0; i < 100; i++){
+	      //resultSet = statement.executeQuery("select * from rmcgarel.Customer");
+	     /*Customer*/
+	      /*for (int i = 7; i < 100; i++){
 		      // preparedStatements can use variables and are more efficient
 		      preparedStatement = connect.prepareStatement("insert into  rmcgarel.Customer (account_num, name, address) values (?, ?, ?)");
-		      // "myuser, webpage, datum, summary, Customers from rmcgarel.Customer");
 		      // parameters start with 1
 		      preparedStatement.setInt(1, i);
 		      preparedStatement.setString(2, Integer.toString(i));
 		      preparedStatement.setString(3, Integer.toString(i));
 		      preparedStatement.executeUpdate();
+	      }*/
+	     /*Cargo*/
+	      /*for (int i = 7; i < 5000; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Cargo (cargo_num, destination) values (?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setString(2, Integer.toString(i));
+	    	  preparedStatement.executeUpdate(); //send it
+	      }*/
+	     /*Airfields*/
+	      /*for (int i = 7; i < 50; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Airfield (airfield_num, name) values (?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setString(2, Integer.toString(i));
+	    	  preparedStatement.executeUpdate();
 	      }
-	      
-	      
+	     /*Aircraft*/
+	      /*for (int i = 7; i < 20; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Aircraft (aircraft_num, destination, location) values (?, ?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setString(2, Integer.toString(i));
+	    	  preparedStatement.setString(3, Integer.toString(i));
+	    	  preparedStatement.executeUpdate();
+	      }
+	     /*Crew*/
+	      /*for (int i = 7; i < 160; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Crew (crew_num, job, name) values (?, ?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setString(2, Integer.toString(i));
+	    	  preparedStatement.setString(3, Integer.toString(i));
+	    	  preparedStatement.executeUpdate();
+	      }
+	     /*Owns*/ 
+	      /*for (int i = 40298; i < 50000; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Owns (account_num, cargo_num) values (?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setInt(2, i);
+	    	  preparedStatement.executeUpdate();
+	      }*/
+	     /*Is_Loaded*/
+	      for (int i = 7; i < 10000; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Is_Loaded (cargo_num, aircraft_num) values (?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setString(2, Integer.toString(i));
+	    	  preparedStatement.executeUpdate();
+	      }
+	     /*Cargo_At*/
+	      for (int i = 7; i < 25000; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Cargo_At (cargo_num, airfield_num) values (?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setInt(2, i);
+	    	  preparedStatement.executeUpdate();
+	      }
+	     /*Is_On_Board*/
+	      for (int i = 7; i < 80; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Is_On_Board (crew_num, aircraft_num) values (?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setInt(2, i);
+	    	  preparedStatement.executeUpdate();
+	      }
+	     /*Crew_At*/
+	      for (int i = 7; i < 80; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Crew_At (crew_num, airfield_num) values (?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setInt(2, i);
+	    	  preparedStatement.executeUpdate();
+	      }
+	     /*Aircraft_At*/
+	      for (int i = 7; i < 80; i++){
+	    	  preparedStatement = connect.prepareStatement("insert into  rmcgarel.Aircraft_At (aircraft_num, airfield_num) values (?, ?)");
+	    	  
+	    	  preparedStatement.setInt(1,i);
+	    	  preparedStatement.setInt(2, i);
+	    	  preparedStatement.executeUpdate();
+	      }
 	    } 
 	    catch (Exception e) {
 	      throw e;
@@ -58,25 +136,6 @@ import java.util.Date;
 	    }
 	  }
 
-	  private void writeResultSet(ResultSet resultSet) throws SQLException {
-	    // resultSet is initialised before the first data set
-	    while (resultSet.next()) {
-	      // it is possible to get the columns via name
-	      // also possible to get the columns via the column number
-	      // which starts at 1
-	      // e.g., resultSet.getSTring(2);
-	      String user = resultSet.getString("myuser");
-	      String website = resultSet.getString("webpage");
-	      String summary = resultSet.getString("summary");
-	      Date date = resultSet.getDate("datum");
-	      String comment = resultSet.getString("comments");
-	      System.out.println("User: " + user);
-	      System.out.println("Website: " + website);
-	      System.out.println("Summary: " + summary);
-	      System.out.println("Date: " + date);
-	      System.out.println("Comment: " + comment);
-	    }
-	  }
 
 	  // you need to close all three to make sure
 	  private void close() {
